8ebff2c413bb67a297ef2d64d7a60317a01c5349
Luis Chilembo Mateus <luischilembomateus@gmail.com>
2024-08-05 16:53:13 +0100

    crud user apartir da api

--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,13 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\Api\UserController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
+Route::get('/v1/users', [UserController::class, 'index'])->name('users.index');
+Route::get('/v1/users/{user}', [UserController::class, 'show'])->name('users.show');
+Route::put('/v1/users/{user}', [UserController::class, 'update'])->name('users.update');
+Route::delete('/users/{user}', [UserController::class, 'destroy'])->name('users.destroy');
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::post('/v1/users', [UserController::class, 'store'])->name('users.store');
+
+Route::get('/v1', fn () => response()->json(['message' => 'ok']));
