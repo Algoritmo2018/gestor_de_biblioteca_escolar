e7715d87e851f471410c48ac599ffaeb996e986e
Luis Chilembo Mateus <luischilembomateus@gmail.com>
2024-08-05 17:36:40 +0100

    crud atores apartir da api

--- routes/api.php
+++ routes/api.php
@@ -1,12 +1,17 @@
 <?php
 
+use App\Http\Controllers\Api\AuthorController;
 use App\Http\Controllers\Api\UserController;
 use Illuminate\Support\Facades\Route;
 
+//Routes author
+Route::apiResource('/authors', AuthorController::class);
+
+// Routes user
 Route::get('/v1/users', [UserController::class, 'index'])->name('users.index');
 Route::get('/v1/users/{user}', [UserController::class, 'show'])->name('users.show');
 Route::put('/v1/users/{user}', [UserController::class, 'update'])->name('users.update');
-Route::delete('/users/{user}', [UserController::class, 'destroy'])->name('users.destroy');
+Route::delete('/v1/users/{user}', [UserController::class, 'destroy'])->name('users.destroy');
 
 Route::post('/v1/users', [UserController::class, 'store'])->name('users.store');
 
